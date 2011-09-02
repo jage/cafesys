51273a672bfc02c8eb823485f4563e8c40e563e0
Simon Pantzare <simon@pewpewlabs.com>
2011-09-02 13:20:26 +0200

    no need for setting comments here

--- cafesys/simon_laptop_settings.py
+++ cafesys/simon_laptop_settings.py
@@ -6,13 +6,13 @@ STATS_CACHE = True
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "cafesys",                       # Or path to database file if using sqlite3.
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "cafesys",
         "TEST_NAME": "test_cafesys",
-        "USER": "sp",                             # Not used with sqlite3.
-        "PASSWORD": "",                         # Not used with sqlite3.
-        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
-        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
+        "USER": "sp",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
 
